491f6b90099438dddc7231d82d2d16789ed97d3c
CybertekSchool <github@cybertekschool.com>
2019-02-07 11:48:44 -0500

    implemented login feature, created page objects and implemented step definitions

---
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "db")
+    public WebElement databaseText;
+
+    @FindBy(id = "login")
+    public WebElement emailInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(css = ".btn-primary")
+    public WebElement loginButton;
+
+}
---
+++ src/test/java/com/cybertek/step_defs/Hooks.java
@@ -0,0 +1,23 @@
+package com.cybertek.step_defs;
+
+import com.cybertek.utilities.Driver;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+
+        Driver.closeDriver();
+
+    }
+
+}
